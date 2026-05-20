7b58ef145e6fd06e17fd49c9568783ee01350e39
em_jeong <merci_emily@naver.com>
2026-05-20 17:31:12 +0900

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 window.HOME_CONFIG = {
   ENDPOINTS: {
     totalAsset:      'https://script.google.com/macros/s/AKfycby6Yqyt5bjnTM2XRO_hMY5W6AK6iwjULcY6H2UN4xnmyQ-Mx2Yho48iI2AWbyw6LKZ6/exec',
-    familyPortfolio: '',
+    familyPortfolio: 'https://script.google.com/macros/s/AKfycbw4ToimVLx0Q_qTwaJyGw0Z0-NBcbl89NrYb3w_aUI_eknj9iJ3CsRMH_fM5kQ7SQcluQ/exec',
     target:          'https://script.google.com/macros/s/AKfycbw4ToimVLx0Q_qTwaJyGw0Z0-NBcbl89NrYb3w_aUI_eknj9iJ3CsRMH_fM5kQ7SQcluQ/exec?action=targets',
     cashflow:        'https://script.google.com/macros/s/AKfycbyNXsSIEwNWPnke1eYcYcm-GTl77-HyH-yi8OrdvwESnb7QQK6fDsqVsrvRy8fdqXzuhw/exec',
     btc:             ''
